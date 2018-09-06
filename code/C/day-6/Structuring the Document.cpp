a903701daca71890f8ae9513fbd975adb8df5f6a
Shubham Chauhan <ShubhamChauhanJi@.github.com>
2018-09-06 14:21:49 -0400

    Hacker Rank Problem Solved

---
+++ code/C/day-6/Structuring the Document.cpp
@@ -0,0 +1,15 @@
+struct document get_document(char* text) {
+
+}
+
+struct word kth_word_in_mth_sentence_of_nth_paragraph(struct document Doc, int k, int m, int n) {
+
+}
+
+struct sentence kth_sentence_in_mth_paragraph(struct document Doc, int k, int m) { 
+
+}
+
+struct paragraph kth_paragraph(struct document Doc, int k) {
+
+}
